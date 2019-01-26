5cc3fa810d5e3e83d7e26067ab711c8ebbf5b8ae
Brian Rossetti <brossetti1@gmail.com>
2019-01-26 14:34:14 -0500

    setting up styled componenets and wrapping theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,62 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import styled from "styled-components"
 
 import Header from './header'
-import './layout.css'
+
+
+const Body = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
+
+const Footer = ({ image }) => (
+  <footer>
+    © {new Date().getFullYear()}, Built with
+    {` `}
+    <span>
+      <div style={{maxWidth: 20}}>
+        <Img fluid={image} />
+      </div>
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </span>
+    <span> and
+      <span style={{color: 'red'}}> &#9829; </span>
+      from Atlanta
+    </span>
+  </footer>
+)
 
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-        file(relativePath: { eq: "gatsby-icon.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
+    query={query}
     render={data => (
-      <>
+      <div>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <div
-            style={{
-              minHeight: 1000
-            }}
-          >
+        <Body>
+          <div style={{ minHeight: 1000 }}>
             {children}
           </div>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <span>
-              <div style={{maxWidth: 20}}>
-                <Img fluid={data.file.childImageSharp.fluid} />
-              </div>
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </span>
-            <span> and
-              <span style={{color: 'red'}}> &#9829; </span>
-              from Atlanta
-            </span>
-          </footer>
-        </div>
-      </>
+          <Footer image={data.file.childImageSharp.fluid}></Footer>
+        </Body>
+      </div>
     )}
   />
 )
@@ -65,5 +51,22 @@ const Layout = ({ children }) => (
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
+
+const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    file(relativePath: { eq: "gatsby-icon.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default Layout
